5e13b9d45b57ced69f15aedb6352fea0ac01f601
Marco <ma.buracchi@gmail.com>
2018-08-11 20:23:12 +0200

    modified precision and error separation

--- primeProbe/primeProbing.c
+++ primeProbe/primeProbing.c
@@ -55,18 +55,19 @@ int main(int argn, char *argv[]) {
 		exit(1);
 	}
 
-	int numberOfTest = strtol(argv[2], NULL, 10); // numero di test
 	int numberOfRuns = strtol(argv[1], NULL, 10); // numero di run per ogni test
+	int numberOfTest = strtol(argv[2], NULL, 10); // numero di test
 	int cacheHitThreshold = strtol(argv[3], NULL, 10); // soglia per cache hit
-	int debug = strtol(argv[4], NULL, 10); // modalita con stampe
+	int precision = strtol(argv[4], NULL, 10); // precisione dei risultati
 
+	int debug = 0; // modalita con stampe
 	int blocks = sizeof(array1[0]) * 8; // numero di bit occupati da ogni posizione dell'array
 	int delta = CACHELINE / blocks; // numero di elementi in una line
-	int precision = 1 * delta; // precisione dei risultati
-	int class = SIZE / precision; // classi di risultati
+	int class = SIZE / delta + 1; // classi di risultati
 
 	int ok = 0; // contatore di OK
 	int error = 0; // contatore di ERROR
+	int noHit = 0;
 
 	int results[class]; // array risultati
 
@@ -98,7 +99,7 @@ int main(int argn, char *argv[]) {
 		// per ogni run
 		for (int j = 0; j < numberOfRuns; j++) {
 
-			for (int l = 1; l < SIZE; l += delta) {
+			for (int l = 1; l <= class; l++) {
 
 				//addestro il branch predictor
 				for (int i = 1; i < 10; i++) {
@@ -122,7 +123,7 @@ int main(int argn, char *argv[]) {
 
 				// calcolo il tempo di accesso alla posizione l
 				time1 = __rdtscp(&timeReg);
-				timeReg = array1[l];
+				timeReg = array1[l * delta];
 				time2 = __rdtscp(&timeReg) - time1;
 
 				for (volatile int z = 0; z < 100; z++) {
@@ -133,12 +134,11 @@ int main(int argn, char *argv[]) {
 				if ((int) time2 <= cacheHitThreshold) {
 					if (debug) {
 						printf("round %d - time = %d results[%d] %d ", j,
-								(int) time2, l / precision,
-								results[l / precision]);
+								(int) time2, l, results[l]);
 					}
-					results[l / precision]++;
+					results[l]++;
 					if (debug) {
-						printf("-> %d\n", results[l / precision]);
+						printf("-> %d\n", results[l]);
 					}
 				}
 			}
@@ -163,25 +163,31 @@ int main(int argn, char *argv[]) {
 		// stampo il risultato confrontando il candidato
 		// con il piu alto numero di occorrenze e lo confronto con il segreto
 		// assicurandomi che ci sia almeno una occorrenza
-		int rangeMax = index * precision + delta;
+		int rangeMax =
+				((index + 1) * delta + (precision * delta) >= SIZE) ?
+						SIZE : (index + 1) * delta + (precision * delta);
 		int rangeMin =
-				(index * precision - delta <= 0) ?
-						0 : index * precision - delta;
+				((index - 1) * delta - (precision * delta) <= 0) ?
+						0 : (index - 1) * delta - (precision * delta);
 
 		if (results[index] > 0 && rangeMin <= secret[userUnderAttack]
 				&& rangeMax >= secret[userUnderAttack]) {
 			printf("OK: predizione tra %d e %d, segreto = %d\n", rangeMin,
 					rangeMax, secret[userUnderAttack]);
 			ok++;
+		} else if (results[index] == 0) {
+			printf("*****NO-HIT: nessuna hit rilevata in %d tentativi *****\n",
+					numberOfRuns);
+			noHit++;
 		} else {
-			printf("*****ERROR: predizione tra %d e %d, segreto = %d *****\n",
-					rangeMin, rangeMax, secret[userUnderAttack]);
+			printf("ERROR: predizione tra %d e %d, segreto = %d\n", rangeMin,
+					rangeMax, secret[userUnderAttack]);
 			error++;
 		}
 	}
 
 // stampo il conteggio totale degli errori e degli ok
-	printf("***TOTALI***\nOK -> %d\nERROR -> %d\n", ok, error);
+	printf("***TOTALI***\nOK -> %d\nNO-HIT -> %d\nERROR -> %d\n", ok, noHit, error);
 
 	return (0);
 }
